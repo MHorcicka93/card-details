268abe0fc7d0b472d1721af38e9646d4a5554291
MHorcicka93 <117664943+MHorcicka93@users.noreply.github.com>
2022-11-21 17:46:25 +0100

    Initial commit

---
+++ component/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- displays site properly based on user's device -->
+
+  <link rel="icon" type="image/png" sizes="32x32" href="./images/favicon-32x32.png">
+  
+  <title>Frontend Mentor | Interactive card details form</title>
+
+  <!-- Feel free to remove these styles or customise in your own stylesheet 👍 -->
+  <style>
+    .attribution { font-size: 11px; text-align: center; }
+    .attribution a { color: hsl(228, 45%, 44%); }
+  </style>
+</head>
+<body>
+<header>
+  <figure class="header__background">
+    <img src="../images/bg-card-back.png" alt="Back Card">
+    <img src="../images/bg-card-front.png" alt="Front Card">
+  </figure>
+</header>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php include "./component/header.php"?>
+    
+
+    </body>
+</html>
